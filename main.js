0a48851427945e88031d174d38b3c041dca5b37d
Mustafa <mostafa.4j@gmail.com>
2022-01-25 23:21:18 +0200

    any

---
+++ main.js
@@ -0,0 +1,12 @@
+// icon hover
+// document.querySelector(".links").addEventListener("mouseenter", enter);
+// document.querySelector(".links").addEventListener("mouseout", out);
+
+// function enter() {
+//   document.querySelector(".icon").children[1].style.width = "100%";
+//   console.log("Hi");
+// }
+// function out() {
+//   document.querySelector(".icon").children[1].style.width = "50%";
+//   console.log("Hi");
+// }
